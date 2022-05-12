aa5b6abe90c6da9df0b4e6623552ed9243a8001f
Ramakant Tiwari <onlinerktiwari@gmail.com>
2022-05-12 21:51:36 +0530

    Edited patternProgram1.c and Created patternProgram1withchar.c

--- patternProgram1withchar.c
+++ patternProgram1withchar.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-void main() 
+int main() 
 {
+    int alphabet = 65;
     // to print * in y-direction
     for (int y_axis = 5; y_axis >= 1; y_axis--)
     {
     // to print * in x-direction
     for(int x_axis = 1; x_axis <= y_axis; x_axis++)
     {
-        printf("*");
+        printf("%c", alphabet);
+        alphabet++;
     }
     printf("\n");   
     }
+
+    return 0;
 }
